c9c0b94141bfcb4493553cd8f232e7b9e0676bf9
Jared4Robertson <jared4robertson@gmail.com>
2019-04-03 20:01:40 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -119,7 +119,8 @@ function draw(){
     // old head position
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-    left.pos(5*(snakeX-food.x),5*(snakeY-food.y),0)
+    //left.pos(5*(snakeX-food.x),5*(snakeY-food.y),0)
+    left.pos(0,0,0)
     // which direction
     if( d == "LEFT") {
         snakeX -= box,d="";
