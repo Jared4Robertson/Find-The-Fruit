7afaedb2f126511785be7ec2b4a82f91f3e467bd
Jared4Robertson <jared4robertson@gmail.com>
2019-04-11 23:54:41 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -120,7 +120,7 @@ function draw(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
     //left.pos(5*(snakeX-food.x),5*(snakeY-food.y),0)
-    left.pos(.04*(food.x-snake[0].x),.04*(snake[0].y-food.y),0)
+    left.pos(.07*(food.x-snake[0].x),.07*(snake[0].y-food.y),0)
     right.pos(.04*(snake[0].x-food.x),.04*(snake[0].y-food.y),0)
     up.pos(.04*(snake[0].x-food.x),.04*(snake[0].y-food.y),0)
     down.pos(.04*(snake[0].x-food.x),.04*(snake[0].y-food.y),0)
